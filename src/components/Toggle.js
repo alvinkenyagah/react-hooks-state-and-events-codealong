863fc025c46520a97e153a5f3808c9319a50420b
alvinkenyagah <alvin47k@gmail.com>
2023-04-21 16:41:02 +0300

    Completed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  useState(true);
+  const [ison, setIson] = useState(true);
+
+  const handleClick = (e) => {
+    setIson(!ison);
+    console.log(ison);
+  };
+  
+
+  const Header = ({ison}) => {
+    return <h1> {ison ? "ON" : "OFF"}</h1>;
+  };
+
+  return(
+  <>
+    <Header ison={ison} />
+    <button onClick={handleClick}>{ison ? "on" : "off"}</button>
+  </>
+  )
 }
 
 export default Toggle;
